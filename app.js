47d866542ebe80d49fbed84f54a2a42114bc0431
nishantkr02 <nishant.nik.02@gmail.com>
2023-05-22 18:55:37 +0530

    feat:Added the first js file

---
+++ app.js
@@ -0,0 +1,12 @@
+/*console.log("Checking");
+//alert("hi there");
+var name =prompt("give me some sunshine");
+alert(`This is my name ${name}`);*/
+
+var translateBtn =document.querySelector("#btn-translate") ;
+/*translateBtn.style.color='red';  */ 
+
+/* How do we interact----- */
+translateBtn.addEventListener("click",function clickHandler(){
+    console.log("clicked");
+})
