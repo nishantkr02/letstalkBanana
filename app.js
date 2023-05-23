2bdd96128ca9706be6aa4b3f2fe39f1b38930389
nishantkr02 <nishant.nik.02@gmail.com>
2023-05-23 13:47:51 +0530

    style;l added the css

--- app.js
+++ app.js
@@ -33,17 +33,29 @@ resetBtn.addEventListener("click",resetHandler);
 
 
 
-var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+/*var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+*/
+var serverURL="https://api.funtranslations.com/translate/minion.json"
 function translationURL(text){
     return serverURL+"?"+"text="+text ;
 }
 
 
+
+function errorHandler(error){
+    console.log("Error Occured",error)
+}
+
 /*For traslate button--------------*/
 function clickHandler(){
  var input=textInput.value ;
+
  fetch(translationURL(input)) 
  .then(response=> response.json())
- .then(json=>console.log(json.contents.translated))
+ .then(json=>{
+    var translatedText=json.contents.translated
+     outputDiv.innerText=translatedText;
+})
+.catch(errorHandler) ;
 }
 translateBtn.addEventListener("click",clickHandler);
