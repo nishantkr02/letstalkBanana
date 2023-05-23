b470828cb8de5821d1e9187a40d0d3e502332377
nishantkr02 <nishant.nik.02@gmail.com>
2023-05-23 10:17:10 +0530

    feat:playing with DOM

--- app.js
+++ app.js
@@ -2,11 +2,48 @@
 //alert("hi there");
 var name =prompt("give me some sunshine");
 alert(`This is my name ${name}`);*/
-
-var translateBtn =document.querySelector("#btn-translate") ;
 /*translateBtn.style.color='red';  */ 
 
-/* How do we interact----- */
-translateBtn.addEventListener("click",function clickHandler(){
-    console.log("clicked");
-})
+
+/*
+textarea tag => document.querySelector("textarea") ;
+buttons with id =>querySelector("#btn-translate")
+element with class btn-primary=> querySelector(".btn-primary");
+input element with attribute name='translator' =>
+querySelector("input[name='translator']");
+*/ 
+
+
+
+
+
+/* How do we interact----- addEventListener("click",callback)*/
+
+var translateBtn =document.querySelector("#btn-translate") ;
+var textInput=document.querySelector("#text-input");
+var outputDiv=document.querySelector("#output");
+var resetBtn=document.querySelector("#reset");
+/*For Reset button-----------------*/
+function resetHandler(){
+    outputDiv.innerHTML='' ;
+    textInput.value='';
+}
+resetBtn.addEventListener("click",resetHandler);
+
+
+
+
+var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+function translationURL(text){
+    return serverURL+"?"+"text="+text ;
+}
+
+
+/*For traslate button--------------*/
+function clickHandler(){
+ var input=textInput.value ;
+ fetch(translationURL(input)) 
+ .then(response=> response.json())
+ .then(json=>console.log(json.contents.translated))
+}
+translateBtn.addEventListener("click",clickHandler);
