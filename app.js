bb6d85f4770adb4cd741b45fe2ff41092f1c90d8
nishantkr02 <134120411+nishantkr02@users.noreply.github.com>
2023-05-28 17:56:22 +0530

    Update app.js
     : Removed some unnecessary comments

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-/*console.log("Checking");
-//alert("hi there");
-var name =prompt("give me some sunshine");
-alert(`This is my name ${name}`);*/
-/*translateBtn.style.color='red';  */ 
-
-
-/*
-textarea tag => document.querySelector("textarea") ;
-buttons with id =>querySelector("#btn-translate")
-element with class btn-primary=> querySelector(".btn-primary");
-input element with attribute name='translator' =>
-querySelector("input[name='translator']");
-*/ 
-
-
-
-
-
-/* How do we interact----- addEventListener("click",callback)*/
 
 var translateBtn =document.querySelector("#btn-translate") ;
 var textInput=document.querySelector("#text-input");
@@ -30,17 +10,12 @@ function resetHandler(){
 }
 resetBtn.addEventListener("click",resetHandler);
 
-
-
-
 /*var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 */
 var serverURL="https://api.funtranslations.com/translate/minion.json"
 function translationURL(text){
     return serverURL+"?"+"text="+text ;
 }
-
-
 
 function errorHandler(error){
     console.log("Error Occured",error)
